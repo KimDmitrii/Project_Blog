f482fafefa8fc60676c676fbe6d238d4d5d293e9
Dmitrii Kim <dmitkim89@gmail.com>
2022-09-28 16:35:05 +0300

    added email and comment parts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -20,12 +21,13 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     # Adding a many-to-one relationship
     # (each is written by a user, and a user can write any number of posts)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    #datetimefields
+    # datetime fields
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -37,14 +39,44 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
-
-    #Defining a default sort order
+    # Defining a default sort order
     class Meta:
         ordering = ['-publish']
-        #adding a database index
+        # adding a database index
         indexes = [
             models.Index(fields=['-publish']),
         ]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                       self.publish.month,
+                       self.publish.day,
+                       self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+   
+
+
